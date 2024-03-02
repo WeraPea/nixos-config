61d695e7b25af15a8a628167170deb9487c91fef
Weronika <sokneip@tuta.io>
2024-03-02 23:25:08 +0100

    generation 191 - nixos

--- home/programs/spicetify.nix
+++ home/programs/spicetify.nix
@@ -2,14 +2,23 @@
   pkgs,
   lib,
   inputs,
+  config,
   ...
 }: let
   spicePkgs = inputs.spicetify-nix.packages.${pkgs.system}.default;
 in {
   imports = [inputs.spicetify-nix.homeManagerModule];
 
+  home.packages = [
+    (pkgs.writeShellScriptBin "spotify" ''
+      export -n NIXOS_OZONE_WL
+      ${config.programs.spicetify.spicedSpotify}/bin/spotify
+    '')
+  ];
+
   programs.spicetify = {
     enable = true;
+    dontInstall = true;
     theme = spicePkgs.themes.text;
     colorScheme = "Spotify";
 
