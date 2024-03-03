bd9cb0acbc49319bba2d7061920502073a32f426
Weronika <sokneip@tuta.io>
2024-03-03 16:21:29 +0100

    generation 192 - nixos

--- home/scripts/default.nix
+++ home/scripts/default.nix
@@ -1,10 +1,26 @@
-{pkgs, ...}: {
-  home.packages = with pkgs;
+{
+  pkgs,
+  lib,
+  ...
+}: {
+  home.packages = with pkgs; let
+    aria2dl = writeShellScriptBin "aria2dl" (builtins.readFile ./aria2dl.sh);
+    aria2dl-desktop-item = makeDesktopItem {
+      name = "aria2dl magnet handler";
+      desktopName = "aria2dl magnet handler";
+      exec = "${lib.getExe aria2dl} %U";
+      # terminal = true;
+      mimeTypes = ["x-scheme-handler/magnet"];
+    };
+    nyaasi = writers.writePython3Bin "nyaasi" {libraries = [pkgs.python3Packages.papis-python-rofi];} (substituteAll {
+      src = ./nyaasi.py;
+      videoPath = "/home/wera/videos";
+    });
+  in
     [
-      (writers.writePython3Bin "nyaasi" {libraries = [pkgs.python3Packages.papis-python-rofi];} (substituteAll {
-        src = ./nyaasi.py;
-        videoPath = "/home/wera/videos";
-      }))
+      aria2dl
+      aria2dl-desktop-item
+      nyaasi
     ]
-    ++ lib.forEach ["0x0" "micmute" "aria2dl" "aria2dl-notify" "cliphist-rofi-img" "search" "rebuild"] (x: writeShellScriptBin "${x}" (builtins.readFile ./${x}.sh));
+    ++ lib.forEach ["0x0" "micmute" "aria2dl-notify" "cliphist-rofi-img" "search" "rebuild"] (x: writeShellScriptBin "${x}" (builtins.readFile ./${x}.sh));
 }
