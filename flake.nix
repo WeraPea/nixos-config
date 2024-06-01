824eb608b1707527b1390aa211fed9a05a5820fb
Weronika <sokneip@tuta.io>
2024-06-01 13:27:29 +0200

    generation 230 - nixos

--- flake.nix
+++ flake.nix
@@ -6,10 +6,10 @@
       flake = false;
     };
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-24.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
     nixvim = {
       url = "github:nix-community/nixvim";
       inputs.nixpkgs.follows = "nixpkgs";
