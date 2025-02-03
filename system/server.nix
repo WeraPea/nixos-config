b29f99753cea4ade73c678dbd6753deb2fafd568
Weronika <sokneip@tuta.io>
2025-02-03 07:54:43 +0100

    generation 5 - server

--- system/server.nix
+++ system/server.nix
@@ -3,4 +3,31 @@
   sql.enable = true;
   graphics.enable = false;
   system.stateVersion = "24.11";
+  # ps2 samba server
+  services.samba = {
+    enable = true;
+    openFirewall = true;
+    settings = {
+      global = {
+        # setting mostly from https://github.com/toolboc/psx-pi-smbshare
+        "server min protocol" = "NT1";
+        "server signing" = "disabled";
+        "smb encrypt" = "disabled";
+        "map to guest" = "bad user";
+        "usershare allow guests" = "yes";
+        "keepalive" = 0;
+        "strict sync" = "no";
+      };
+      ps2 = {
+        comment = "PS2 SMB";
+        path = "/ps2samba";
+        browseable = "yes";
+        "read only" = "no";
+        "guest ok" = "yes";
+        public = "yes";
+        available = "yes";
+        "force user" = "wera";
+      };
+    };
+  };
 }
