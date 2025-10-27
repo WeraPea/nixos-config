cac49e1f693c13a09f4038769bc6fa41e041bed0
werapi <sokneip@tuta.io>
2025-10-27 21:13:50 +0100

    Revert "change multimonitor binds for tag switching"
    This reverts commit 155cead680064e7c7d5f4ace73a917fea190538c.

--- home/nixos.nix
+++ home/nixos.nix
@@ -43,14 +43,29 @@
               monitorrule=HDMI-A-1,0.5,1,tile,0,1,${toString HDMI_A_1_x},${toString HDMI_A_1_y},${toString HDMI_A_1_width},${toString HDMI_A_1_height},75,0,0,0,0
               monitorrule=HDMI-A-2,0.5,1,tile,0,1,${toString HDMI_A_2_x},${toString HDMI_A_2_y},${toString HDMI_A_2_width},${toString HDMI_A_2_height},60,${toString HDMI_A_2_overscan_top},${toString HDMI_A_2_overscan_bottom},${toString HDMI_A_2_overscan_left},${toString HDMI_A_2_overscan_right}
 
-              bind=SUPER,F1,focusmon,DP-2
+              bind=SUPER,F1,focusmon,HDMI-A-1
+              bind=SUPER,F1,comboview,1
               bind=SUPER,F2,focusmon,HDMI-A-1
-              bind=SUPER,F3,focusmon,HDMI-A-2
+              bind=SUPER,F2,comboview,2
+              bind=SUPER,F3,focusmon,HDMI-A-1
+              bind=SUPER,F3,comboview,3
+              bind=SUPER,F4,focusmon,HDMI-A-2
+              bind=SUPER,F4,comboview,1
+              bind=SUPER,F5,focusmon,HDMI-A-2
+              bind=SUPER,F5,comboview,2
 
-              bind=SUPER+SHIFT,F1,tagmon,DP-2
+              bind=SUPER+SHIFT,F1,tagmon,HDMI-A-1
+              bind=SUPER+SHIFT,F1,tag,1
               bind=SUPER+SHIFT,F2,tagmon,HDMI-A-1
-              bind=SUPER+SHIFT,F3,tagmon,HDMI-A-2
+              bind=SUPER+SHIFT,F2,tag,2
+              bind=SUPER+SHIFT,F3,tagmon,HDMI-A-1
+              bind=SUPER+SHIFT,F3,tag,3
+              bind=SUPER+SHIFT,F4,tagmon,HDMI-A-2
+              bind=SUPER+SHIFT,F4,tag,1
+              bind=SUPER+SHIFT,F5,tagmon,HDMI-A-2
+              bind=SUPER+SHIFT,F5,tag,2
             '';
+          mainDisplay = "DP-2";
         };
         quickshell.enable = true;
       }
