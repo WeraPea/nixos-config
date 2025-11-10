cad02034e594b994ebc92929368fe6c00dc78133
werapi <sokneip@tuta.io>
2025-11-10 18:40:18 +0100

    lower pipewire latency

--- system/services.nix
+++ system/services.nix
@@ -27,6 +27,9 @@
         "10-clock-rate" = {
           "context.properties" = {
             "default.clock.rate" = 48000;
+            "default.clock.quantum" = 32;
+            "default.clock.min-quantum" = 32;
+            "default.clock.max-quantum" = 32;
           };
         };
       };
