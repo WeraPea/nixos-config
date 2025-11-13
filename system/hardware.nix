fb40147dd10f2b1e6e72ae58971862147a49755d
werapi <sokneip@tuta.io>
2025-11-13 10:31:44 +0100

    add bluetooth-resume-cleanup

--- system/hardware.nix
+++ system/hardware.nix
@@ -1,11 +1,25 @@
-{ config, lib, ... }:
 {
-  hardware = lib.mkIf config.graphics.enable {
+  pkgs,
+  config,
+  lib,
+  ...
+}:
+lib.mkIf config.graphics.enable {
+  hardware = {
     bluetooth.enable = true;
     keyboard.qmk.enable = true;
     opentabletdriver.enable = true;
     xpadneo.enable = lib.mkIf config.gaming.enable true;
     graphics.enable = true;
     graphics.enable32Bit = lib.mkDefault true;
+  };
+  systemd.services.bluetooth-resume-cleanup = {
+    description = "Restart bluetooth after resume to remove stale devices from UPower";
+    wantedBy = [ "post-resume.target" ];
+    after = [ "post-resume.target" ];
+    serviceConfig = {
+      Type = "oneshot";
+      ExecStart = "${pkgs.systemd}/bin/systemctl restart bluetooth.service";
+    };
   };
 }
