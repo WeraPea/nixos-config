2223f5883e41f3c46129bdbe7b49c13ff8480ac0
werapi <sokneip@tuta.io>
2025-11-13 11:29:06 +0100

    Revert "lower pipewire latency"
    This reverts commit cad02034e594b994ebc92929368fe6c00dc78133.

--- system/services.nix
+++ system/services.nix
@@ -27,9 +27,6 @@
         "10-clock-rate" = {
           "context.properties" = {
             "default.clock.rate" = 48000;
-            "default.clock.quantum" = 32;
-            "default.clock.min-quantum" = 32;
-            "default.clock.max-quantum" = 32;
           };
         };
       };
