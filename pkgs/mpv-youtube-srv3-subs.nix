98f398548e9c97fbf9f8e0d0f57ec2b05a5b7508
werapi <sokneip@tuta.io>
2026-01-22 22:55:52 +0100

    improve mpv-youtube-srv3-subs to work with auto-generated subtitles

--- pkgs/mpv-youtube-srv3-subs.nix
+++ pkgs/mpv-youtube-srv3-subs.nix
@@ -4,7 +4,7 @@
   mpvScripts,
 }:
 let
-  version = "2026.01.05";
+  version = "2026.01.22";
   script =
     builtins.toFile "youtube-srv3-subs.lua" # lua
       ''
@@ -23,58 +23,91 @@ let
           end
         end
 
+        local function strip_backgrounds(ass_file)
+            local f = io.open(ass_file, "r")
+            if not f then return end
+            local content = f:read("*all")
+            f:close()
+
+            -- Change BorderStyle from 3 to 1 AND set Outline from 0.01 to 2
+            content = content:gsub("(Style: YT%w*Box,[^,]+,[^,]+,[^,]+,[^,]+,[^,]+,[^,]+,[^,]+,[^,]+,[^,]+,[^,]+,[^,]+,[^,]+,[^,]+,[^,]+,)3,([%d%.]+)", "%11,2")
+            -- sadly the border will appear before the karaoke timings, but still better than a whole box doing so too
+
+            f = io.open(ass_file, "w")
+            f:write(content)
+            f:close()
+        end
+
+        local function build_ytdlp_base_cmd(path, tmpdir_path)
+            local cmd = {
+                "yt-dlp",
+                "--skip-download",
+                "--write-subs",
+                "--sub-langs", "en",
+                "--sub-format", "srv3",
+                "-o", tmpdir_path .. "/subs.%(ext)s",
+                path
+            }
+
+            if o.cookies_from_browser ~= "" then
+              table.insert(cmd, "--cookies-from-browser")
+              table.insert(cmd, o.cookies_from_browser)
+            elseif o.cookies ~= "" then
+              table.insert(cmd, "--cookies")
+              table.insert(cmd, o.cookies)
+            end
+
+            return cmd
+        end
+
         mp.register_event("start-file", function()
             local path = mp.get_property("path")
             if not path or not path:match("^https?://") then return end
             if not path:match("youtube%.com") and not path:match("youtu%.be") then return end
-            local mktemp_res = utils.subprocess({args = {"mktemp", "-d"}})
 
+            local mktemp_res = utils.subprocess({args = {"mktemp", "-d"}})
             if mktemp_res.status ~= 0 or not mktemp_res.stdout then
               mp.msg.error("Failed to create temporary directory")
               return
             end
             tmpdir = mktemp_res.stdout:gsub("%s+$", "")
 
+            local manual_cmd = build_ytdlp_base_cmd(path, tmpdir)
 
             local srv3_path = tmpdir .. "/subs.en.srv3"
             local sub_ass_path = tmpdir .. "/subs.en.ass"
+            local has_manual = false
 
-            local ytdlp_cmd = {
-                "yt-dlp",
-                "--skip-download",
-                "--write-subs",
-                "--sub-langs", "en",
-                "--sub-format", "srv3",
-                "-o", tmpdir .. "/subs.%(ext)s",
-                path
-            }
-            if o.cookies_from_browser ~= "" then
-              table.insert(ytdlp_cmd, "--cookies-from-browser")
-              table.insert(ytdlp_cmd, o.cookies_from_browser)
-            elseif o.cookies ~= "" then
-              table.insert(ytdlp_cmd, "--cookies")
-              table.insert(ytdlp_cmd, o.cookies)
+            if utils.file_info(srv3_path) ~= nil then
+                local convert_cmd = {"YTSubConverter", srv3_path}
+                local convert_res = utils.subprocess({args = convert_cmd})
+
+                if convert_res.status == 0 then
+                    has_manual = true
+                    mp.commandv("sub-add", sub_ass_path)
+                    mp.msg.info("Loaded manual subtitles")
+                end
             end
 
-            utils.subprocess({args = ytdlp_cmd})
+            if not has_manual then
+                os.execute("rm -f " .. srv3_path)
+                os.execute("rm -f " .. sub_ass_path)
 
-            if utils.file_info(srv3_path) == nil then
-              safe_rm(tmpdir)
-              return
+                local auto_cmd = build_ytdlp_base_cmd(path, tmpdir)
+                table.insert(auto_cmd, "--write-auto-subs")
+                utils.subprocess({args = auto_cmd})
+
+                if utils.file_info(srv3_path) ~= nil then
+                    local convert_cmd = {"YTSubConverter", srv3_path}
+                    local convert_res = utils.subprocess({args = convert_cmd})
+
+                    if convert_res.status == 0 then
+                        strip_backgrounds(sub_ass_path)
+                        mp.commandv("sub-add", sub_ass_path)
+                        mp.msg.info("Loaded auto-generated subtitles (backgrounds stripped)")
+                    end
+                end
             end
-
-            local convert_cmd = {
-                "YTSubConverter",
-                srv3_path
-            }
-            local convert_res = utils.subprocess({args = convert_cmd})
-            if convert_res.status ~= 0 then
-              mp.msg.error("Subtitle conversion failed: " .. (convert_res.stderr or "unknown error"))
-              safe_rm(tmpdir)
-              return
-            end
-
-            mp.commandv("sub-add", sub_ass_path)
         end)
 
         mp.register_event("end-file", function()
