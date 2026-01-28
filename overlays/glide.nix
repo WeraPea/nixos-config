809b682ccb792e4a4773fcae2b34738f6597989e
werapi <sokneip@tuta.io>
2026-01-28 09:46:02 +0100

    improve glide overlay

--- overlays/glide.nix
+++ overlays/glide.nix
@@ -8,64 +8,60 @@ final: prev: {
       extraPoliciesFiles ? [ ],
       ...
     }:
+    let
+      policiesJson = final.writeText "policies.json" (
+        builtins.toJSON {
+          policies = {
+            DisableAppUpdate = true;
+          }
+          // extraPolicies;
+        }
+      );
+
+      mozillaCfg = ''
+        // mozilla.cfg
+      '';
+    in
     prev.glide-browser.overrideAttrs (oldAttrs: {
       nativeBuildInputs = (oldAttrs.nativeBuildInputs or [ ]) ++ [ final.jq ];
 
       postInstall = (oldAttrs.postInstall or "") + ''
         libDir="$out/lib/glide-browser-bin-${oldAttrs.version}"
         mkdir -p "$libDir/distribution"
+
         POL_PATH="$libDir/distribution/policies.json"
+        rm -f "$POL_PATH"
+        cat ${policiesJson} > "$POL_PATH"
 
-        cat > "$POL_PATH" << 'EOF'
-        {
-          "policies": {
-            "DisableAppUpdate": true
-          }
-        }
-        EOF
-
-        ${final.lib.optionalString (extraPolicies != { }) ''
-          cat > .extra-policies.json << 'EXTRAPOL'
-          ${builtins.toJSON { policies = extraPolicies; }}
-          EXTRAPOL
-
-          jq -s '.[0].policies * .[1].policies | {policies: .}' "$POL_PATH" .extra-policies.json > .tmp.json
+        extraPoliciesFiles=(${toString extraPoliciesFiles})
+        for extraPoliciesFile in "''${extraPoliciesFiles[@]}"; do
+          jq -s '.[0] * .[1]' $extraPoliciesFile "$POL_PATH" > .tmp.json
           mv .tmp.json "$POL_PATH"
-          rm .extra-policies.json
-        ''}
+        done
 
-        ${final.lib.concatMapStringsSep "\n" (file: ''
-          jq -s '.[0] * .[1]' "$POL_PATH" "${file}" > .tmp.json
-          mv .tmp.json "$POL_PATH"
-        '') extraPoliciesFiles}
-
-        # autoconfig hook
+        # preparing for autoconfig
         prefsDir="$libDir/defaults/pref"
         mkdir -p "$prefsDir"
 
-        cat > "$prefsDir/autoconfig.js" << 'EOF'
-        pref("general.config.filename", "mozilla.cfg");
-        pref("general.config.obscure_value", 0);
+        echo 'pref("general.config.filename", "mozilla.cfg");' > "$prefsDir/autoconfig.js"
+        echo 'pref("general.config.obscure_value", 0);' >> "$prefsDir/autoconfig.js"
+
+        cat >> "$libDir/mozilla.cfg" << EOF
+        ${extraPrefs}
         EOF
 
-        # main prefs file
-        CFG_PATH="$libDir/mozilla.cfg"
-        rm -f "$CFG_PATH"
+        cat > "$libDir/mozilla.cfg" << EOF
+        ${mozillaCfg}
+        EOF
 
-        # first line must be a comment
-        echo '// mozilla.cfg' > "$CFG_PATH"
+        extraPrefsFiles=(${toString extraPrefsFiles})
+        for extraPrefsFile in "''${extraPrefsFiles[@]}"; do
+          cat "$extraPrefsFile" >> "$libDir/mozilla.cfg"
+        done
 
-        # inline prefs
-        ${final.lib.optionalString (extraPrefs != "") ''
-            cat >> "$CFG_PATH" << 'EOF'
-          ${extraPrefs}
-          EOF
-        ''}
-
-        # prefs files
-        ${final.lib.concatMapStringsSep "\n" (file: ''
-          cat "${file}" >> "$CFG_PATH"
-        '') extraPrefsFiles}
+        cat >> "$libDir/mozilla.cfg" << EOF
+        ${extraPrefs}
+        EOF
       '';
     })
   ) { };
