a7bae067ac5427bb6f0ac47de94519fdbe40f1ad
werapi <sokneip@tuta.io>
2026-06-20 20:45:23 +0200

    move mkRemoteWrapper to overlays

--- modules/overlays.nix
+++ modules/overlays.nix
@@ -139,6 +139,40 @@ in
             }) { system = final.stdenv.hostPlatform.system; };
           in
           old-nixpkgs.nh; # until nh gives me remote copy progress back
+
+        werapi = (prev.werapi or { }) // {
+          mkRemoteWrapper = final.callPackage (
+            {
+              lib,
+              openssh,
+              writeShellScriptBin,
+            }:
+            {
+              hostname,
+              targetHostname,
+              targetSsh ? targetHostname,
+              package ? null, # the derivation that we have to assume the target machine has in store
+              name ?
+                if (package != null) then
+                  package.meta.mainProgram or (lib.getName package)
+                else
+                  lib.tail (lib.splitString "/" exe),
+              exe ? lib.getExe' package name,
+            }:
+            writeShellScriptBin "${name}-remote-wrapped" (
+              if hostname == targetHostname then # sh
+                ''
+                  exec "${exe}" "$@"
+                '' # sh
+              else
+                ''
+                  exe="${builtins.unsafeDiscardStringContext exe}"
+                  remote_cmd=$(printf '%q ' "$exe" "$@")
+                  ${lib.getExe openssh} ${targetSsh} "sh -c $(printf '%q ' "$remote_cmd")"
+                ''
+            )
+          ) { };
+        };
       })
     ];
   };
