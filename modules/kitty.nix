3373db65235418de4c58627f809152e9c668e36a
werapi <sokneip@tuta.io>
2026-07-12 11:24:30 +0200

    move kitty to a wrapper

--- modules/kitty.nix
+++ modules/kitty.nix
@@ -1,7 +1,55 @@
+{
+  flake,
+  ...
+}:
 let
   moduleName = "kitty";
 in
 {
+  flake.wrappers.kitty =
+    {
+      lib,
+      pkgs,
+      wlib,
+      ...
+    }:
+    {
+      imports = [
+        wlib.wrapperModules.kitty
+      ];
+      keybindings = {
+        "ctrl+shift+n" = "new_tab !neighbor";
+        "ctrl+shift+m" = "new_tab_with_cwd !neighbor";
+
+        "alt+h" = "previous_tab";
+        "alt+l" = "next_tab";
+
+        "shift+alt+h" = "move_tab_backward";
+        "shift+alt+l" = "move_tab_forward";
+
+        "ctrl+shift+h" = "previous_tab";
+        "ctrl+shift+l" = "next_tab";
+
+        "kitty_mod+k" = "change_font_size all +1.0";
+        "kitty_mod+j" = "change_font_size all -1.0";
+      };
+      settings = {
+        placement_strategy = "top-left";
+        shell_integration = "no-sudo";
+
+        confirm_os_window_close = 2;
+        allow_remote_control = "yes";
+
+        cursor = "none";
+
+        tab_bar_edge = "top";
+        tab_bar_min_tabs = 1;
+        tab_bar_style = "powerline";
+        tab_title_template = "{fmt.fg.red}{bell_symbol}{activity_symbol}{fmt.fg.tab}{index}:{title[:15]}";
+        clipboard_control = "write-clipboard write-primary read-clipboard read-primary";
+        scrollback_pager = lib.getExe pkgs.nvimpager;
+      };
+    };
   flake.modules.${moduleName}.nixos =
     {
       config,
@@ -12,6 +60,9 @@ in
       cfg = config.werapi.${moduleName};
     in
     {
+      imports = [
+        flake.wrappers.kitty.install
+      ];
       options.werapi.${moduleName} = {
         enable = lib.mkOption {
           default = config.werapi.graphics.enable;
@@ -28,42 +79,9 @@ in
         in
         with config.lib.stylix.colors.withHashtag;
         lib.mkIf cfg.enable {
-          hm.stylix.targets.kitty.enable = false;
-          hm.programs.kitty = {
-            enable = true;
-            shellIntegration.mode = "no-sudo";
-            keybindings = {
-              "ctrl+shift+n" = "new_tab !neighbor";
-              "ctrl+shift+m" = "new_tab_with_cwd !neighbor";
-
-              "alt+h" = "previous_tab";
-              "alt+l" = "next_tab";
-
-              "shift+alt+h" = "move_tab_backward";
-              "shift+alt+l" = "move_tab_forward";
-
-              "ctrl+shift+h" = "previous_tab";
-              "ctrl+shift+l" = "next_tab";
-
-              "kitty_mod+k" = "change_font_size all +1.0";
-              "kitty_mod+j" = "change_font_size all -1.0";
-            };
-            settings = {
-              placement_strategy = "top-left";
-
-              confirm_os_window_close = 2;
-              allow_remote_control = "yes";
-
-              cursor = "none";
-
-              tab_bar_edge = "top";
-              tab_bar_min_tabs = 1;
-              tab_bar_style = "powerline";
-              tab_title_template = "{fmt.fg.red}{bell_symbol}{activity_symbol}{fmt.fg.tab}{index}:{title[:15]}";
-              clipboard_control = "write-clipboard write-primary read-clipboard read-primary";
-            };
+          wrappers.kitty = {
             font = {
-              inherit (config.stylix.fonts.monospace) package name;
+              inherit (config.stylix.fonts.monospace) name;
               size = config.stylix.fonts.sizes.terminal;
             };
             settings.background_opacity = with config.stylix.opacity; "${toString terminal}";
@@ -76,6 +94,7 @@ in
               tab_bar_background      ${base00}
             '';
           };
+          environment.systemPackages = [ config.wrappers.kitty.wrapper ];
         };
     };
 }
