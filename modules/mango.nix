9f19b9bedd7af96282481942654a02aed3ac1dfa
werapi <sokneip@tuta.io>
2026-09-22 07:11:04 +0200

    add mpvplaypausefocus bind

--- modules/mango.nix
+++ modules/mango.nix
@@ -193,6 +193,46 @@ in
             "SUPER,b" = "toggle_render_border";
             "SUPER,t" =
               "spawn_shell,glide --new-window https://renji-xd.github.io/texthooker-ui/; sleep 1; mmsg dispatch togglefloating; mmsg dispatch toggleoverlay; mmsg dispatch togglefakefullscreen; mmsg dispatch resizewin,540,820; mmsg dispatch movewin,3842,0";
+            "SUPER,f" = "spawn,${pkgs.writeShellScript "mpvplaypausefocus" ''
+              pidfile="/tmp/$(basename "$0").pid"
+              export PATH="${lib.makeBinPath [ pkgs.libnotify ]}:$PATH"
+              if [[ -f "$pidfile" ]] && kill -0 "$(cat "$pidfile")" 2>/dev/null; then
+                kill "$(cat "$pidfile")"
+                notify-send off -t 1000
+                rm -f "$pidfile"
+                exit 0
+              fi
+              notify-send on -t 1000
+
+              echo $$ > "$pidfile"
+
+              cleanup() {
+                kill "$mmsg_pid" "$jq_pid" 2>/dev/null
+                rm -f "$tmp" "$pidfile"
+              }
+              trap cleanup EXIT INT TERM
+
+              tmp=$(mktemp -u)
+              mkfifo "$tmp"
+
+              mmsg watch all-monitors >"$tmp" &
+              mmsg_pid=$!
+              jq -r --unbuffered '.monitors[] | select(.active) | .active_client.appid' <"$tmp" | while read -r client; do
+                if [ "$client" = "mpv" ]; then
+                  if [ "$prev" != "mpv" ]; then
+                    playerctl -p mpv play
+                  fi
+                  prev=mpv
+                else
+                  if [ "$prev" == "mpv" ]; then
+                    playerctl -p mpv pause
+                  fi
+                  prev=notmpv
+                fi
+              done &
+              jq_pid=$!
+              wait "$jq_pid"
+            ''}";
           };
         };
         clipboard = mango-lib.mkClipboardMode {
